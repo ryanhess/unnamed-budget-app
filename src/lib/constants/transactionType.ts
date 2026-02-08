b8088b2e6b1aef3e18f08a951f18e284867ed80c
ryanhess <hess29@gmail.com>
2026-02-08 16:46:17 -0500

    Added Zod Schema

--- src/lib/constants/transactionType.ts
+++ src/lib/constants/transactionType.ts
@@ -1,12 +1,19 @@
-type Transaction = {
-    id: string;
-    date: string;
-    merchant: string;
-    category: string;
-    amount: number;
-    type: "income" | "expense";
-    icon: React.ReactNode;
-    accountId: string;
+import { z } from "zod";
+import { ReactNode } from "react";
+
+const TransactionSchema = z.object({
+    id: z.string(),
+    date: z.string(),
+    merchant: z.string(),
+    category: z.string(),
+    amount: z.number(),
+    accountId: z.string(),
+    type: z.enum(["income", "expense"]),
+});
+
+type Transaction = z.infer<typeof TransactionSchema> & {
+    icon: ReactNode;
 };
 
 export type { Transaction };
+export { TransactionSchema };
