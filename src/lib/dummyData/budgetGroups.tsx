a28c0546a4f2e707ce49c652479d8efdbbee9b3f
ryanhess <hess29@gmail.com>
2026-02-11 00:37:35 -0500

    WIP

--- src/lib/dummyData/budgetGroups.tsx
+++ src/lib/dummyData/budgetGroups.tsx
@@ -1,4 +1,5 @@
 import type { BudgetGroup } from "@/lib/constants";
+import { getBudgetItemById } from "@/lib/dummyData/budgetItems";
 
 const budgetGroups: BudgetGroup[] = [
     {
@@ -27,3 +28,35 @@ const budgetGroups: BudgetGroup[] = [
         items: ["9", "10"],
     },
 ];
+
+// prettier-ignore
+const sumOneAssignedOrSpent = (field: "assigned" | "spent") =>
+    (sum: number, itemId: string): number => {
+        const item = getBudgetItemById(itemId);
+        return sum + (item?.[field] ?? 0);
+    };
+
+//prettier-ignore
+const getAllBudgetGroupIds = (): string[] =>
+    budgetGroups.map((cat: BudgetGroup) => cat.id);
+
+const getBudgetGroupById = (id: string): BudgetGroup | null =>
+    budgetGroups.find((group) => group.id === id) || null;
+
+// Fail silently here.
+const getTotalAssignedForGroup = (id: string): number => {
+    const group = getBudgetGroupById(id);
+    return group?.items.reduce(sumOneAssignedOrSpent("assigned"), 0) ?? 0;
+};
+
+const getTotalSpentForGroup = (id: string): number => {
+    const group = getBudgetGroupById(id);
+    return group?.items.reduce(sumOneAssignedOrSpent("spent"), 0) ?? 0;
+};
+
+export {
+    getAllBudgetGroupIds,
+    getBudgetGroupById,
+    getTotalAssignedForGroup,
+    getTotalSpentForGroup,
+};
