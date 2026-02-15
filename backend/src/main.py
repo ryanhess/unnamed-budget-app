bbd2eaf229c996e8ce404d0aa745f8c80714946e
ryanhess <hess29@gmail.com>
2026-02-15 18:45:33 -0500

    setup api route endpoint stubs for all txns

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from transactions.router import router as transactions_routes
 
 app = FastAPI()
 
+app.include_router(transactions_routes, prefix="/transactions")
+
 @app.get("/")
 async def serve_root():
-    return {"message": "Hello, World!"}
+    return {"message": "TEST! Hello World"}
