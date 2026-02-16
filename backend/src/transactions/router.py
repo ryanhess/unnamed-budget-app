cc7eeec2cf2d31303780b0f60461c3d79cff2cef
ryanhess <hess29@gmail.com>
2026-02-15 19:15:42 -0500

    added type annotations

--- backend/src/transactions/router.py
+++ backend/src/transactions/router.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("")
-def get_all_transactions():
+@router.get("/")
+def get_all_transactions() -> dict[str, str]:
     return {"message": "all transactions"}
+
+@router.get("/{bankAccountId}")
+def get_specific_account_transactions(bankAccountId: str) -> dict[str, str]:
+    return {"message": f'got id: {bankAccountId}'}
