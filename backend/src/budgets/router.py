527455252e8b46e01bf1fdd3ca87b30ab99f4654
ryanhess <hess29@gmail.com>
2026-02-24 08:47:13 -0500

    updated route, the same routes work now.

--- backend/src/budgets/router.py
+++ backend/src/budgets/router.py
@@ -3,22 +3,24 @@ from src.budgets.models import BudgetGroup, BudgetGroupOrm
 from src.budgets.models import BudgetItem, BudgetItemOrm
 from src.database import get_db
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload
+from sqlalchemy.orm import selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
+
 
 router = APIRouter()
 
-# Joinedload as an option here overrides the lazy loading default
-# behavior with the budget_items attribute. Before, there were issues
-# where SqlAlchemy needed to query the db at serialization
-# time to lazy-load budget_items in each budget_group,
-# but since that is outside the scope there is no async connection
-# context to run the query in -> runtime error. 
+
+# Leaving this option out would result in many queries being run at serialization time
+# in order to fill out the items for each group. This causes only one additional
+# query to run so that everything is filled out. Anyway, the async engine would have
+# resulted in runtime errors when those serialization queries run, so this option
+# solves two problems: efficiency, and runtime error for async.
 @router.get("/groups/getall", response_model=list[BudgetGroup])
 async def get_all_groups_for_user_budget(db: AsyncSession=Depends(get_db)):
-    query = select(BudgetGroupOrm).options(joinedload(BudgetGroupOrm.budget_items))
+    query = select(BudgetGroupOrm).options(selectinload(BudgetGroupOrm.budget_items))
     result = await db.execute(query)
     return result.scalars().all()
+
 
 @router.get("/items/{budget_item_id}", response_model=BudgetItem)
 async def get_budget_item(budget_item_id: str, db: AsyncSession=Depends(get_db)):
