d3e41224c361f34e3aeda4adcc0eb6c517e1e532
ryanhess <hess29@gmail.com>
2026-03-15 16:41:47 -0400

    Debugged results: - fixed the migration file to setup the date conversion of existing data. - fixed the foreign key relationship between transactions and budget items

--- backend/src/transactions/models.py
+++ backend/src/transactions/models.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
 from enum import Enum
-from sqlalchemy import Enum as SqlAlchEnum, ForeignKey, Identity, Date
+from sqlalchemy import (
+    Enum as SqlAlchEnum,
+    ForeignKey,
+    Identity,
+    Date as sqlalchemy_date,
+)
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import OrmBase
 from datetime import date as date_type
-from budgets.models import BudgetItemOrm
+from src.budgets.models import BudgetItemOrm
 
 
 class TransactionType(str, Enum):
@@ -40,14 +45,14 @@ class TransactionOrm(OrmBase):
     __tablename__ = "transactions"
 
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
-    date: Mapped[date_type] = mapped_column(Date)
+    date: Mapped[date_type] = mapped_column(sqlalchemy_date)
 
     merchant: Mapped[str]
     category: Mapped[str]
-    budget_item_id: Mapped[int] = mapped_column(nullable=True)
-    budget_item: Mapped[BudgetItemOrm | None] = relationship(
-        back_populates="transactions"
+    budget_item_id: Mapped[int | None] = mapped_column(
+        ForeignKey("budget_items.id", ondelete="SET NULL"), nullable=True
     )
+    budget_item: Mapped[BudgetItemOrm | None] = relationship()
     amount: Mapped[float]
     type: Mapped[TransactionType] = mapped_column(SqlAlchEnum(TransactionType))
 
