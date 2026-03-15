93339f9da4ec17e6684f871df5e8e53a3c0cdd27
ryanhess <hess29@gmail.com>
2026-03-15 14:53:57 -0400

    Altered TransactionOrm and BudgetItemOrm: - date is a date object - column that establishes relationship between budget items and transactions - did not delete the "category" column to minimize diff and impact the workflow, that's for another PR

--- backend/src/transactions/models.py
+++ backend/src/transactions/models.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel
 from enum import Enum
-from sqlalchemy import Enum as SqlAlchEnum, ForeignKey, Identity
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Enum as SqlAlchEnum, ForeignKey, Identity, Date
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from src.database import OrmBase
+from datetime import date as date_type
+from budgets.models import BudgetItemOrm
 
 
 class TransactionType(str, Enum):
@@ -38,9 +40,14 @@ class TransactionOrm(OrmBase):
     __tablename__ = "transactions"
 
     id: Mapped[int] = mapped_column(Identity(always=True), primary_key=True)
-    date: Mapped[str]
+    date: Mapped[date_type] = mapped_column(Date)
+
     merchant: Mapped[str]
     category: Mapped[str]
+    budget_item_id: Mapped[int] = mapped_column(nullable=True)
+    budget_item: Mapped[BudgetItemOrm | None] = relationship(
+        back_populates="transactions"
+    )
     amount: Mapped[float]
     type: Mapped[TransactionType] = mapped_column(SqlAlchEnum(TransactionType))
 
