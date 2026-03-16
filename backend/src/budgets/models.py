c112dd1901e89de2b115635b7d1106f4405a3731
Ryan Hess <ryanhess@users.noreply.github.com>
2026-03-16 00:48:55 -0400

    added calculated fields to group schema that will produce the total assigned, spent, and avalable for the envelope objects of the items. (#88)

--- backend/src/budgets/models.py
+++ backend/src/budgets/models.py
@@ -85,6 +85,21 @@ class BudgetGroupResponse(BaseModel):
     # Defined below, so forward import
     budget_items: list["BudgetItemResponse"]
 
+    @computed_field
+    @property
+    def total_assigned(self) -> float:
+        return sum(item.envelope.assigned for item in self.budget_items)
+
+    @computed_field
+    @property
+    def total_spent(self) -> float:
+        return sum(item.envelope.spent for item in self.budget_items)
+
+    @computed_field
+    @property
+    def total_available(self) -> float:
+        return sum(item.envelope.available for item in self.budget_items)
+
     class Config:
         from_attributes = True
 
